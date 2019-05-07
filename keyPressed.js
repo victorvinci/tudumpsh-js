7b1e00b4f25ee449796f3aa718f5e3a91cf5b8ff
victor vinci <victorvinci@protonmail.com>
2019-05-07 18:31:10 +0200

    fix typo in html and play audio when key is pressed

--- keyPressed.js
+++ keyPressed.js
@@ -1,4 +1,11 @@
 window.addEventListener('keydown', function (event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  
+  if(!audio) {
+    return
+  } else {
+    audio.play();
+  };
+
   console.log(audio);
 }); 
