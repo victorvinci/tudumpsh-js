b4b27e6148e29741c618673052dd7f8667bb973c
victor vinci <victorvinci@protonmail.com>
2019-05-07 22:17:23 +0200

    add loop effect with red feedback

--- keyPressed.js
+++ keyPressed.js
@@ -4,6 +4,16 @@ function playSound(keyPressed) {
 
   if (!audio) {
     return;
+
+  } else if (audio.loop && audio.paused){ // check if audio has loop option and is not playing to start it
+    audio.play();
+    key.classList.add('playing-red'); // add a red feedback for looping sounds that are playing
+
+  } else if (audio.loop && !audio.paused){ // check if the audio is playing to pause it
+    audio.pause();
+    audio.currentTime = 0;
+    key.classList.remove('playing-red');
+
   } else {
     audio.currentTime = 0; // currentTime is used in the <audio> HTML tag to reset the audio whenever key is pressed.
     audio.play();
@@ -15,7 +25,7 @@ function playSound(keyPressed) {
   if (keyPressed.propertyName !== 'transform') {
     return;
   } else {
-    this.classList.remove('playing');
+    this.classList.remove('playing');;
   }
 }
 
