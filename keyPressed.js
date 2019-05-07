67431ea16e4435b54398fee06147bfa3ce00c1c9
victor vinci <victorvinci@protonmail.com>
2019-05-07 19:35:54 +0200

    add and remove class to feedback which key was pressed/ is playing

--- keyPressed.js
+++ keyPressed.js
@@ -1,12 +1,23 @@
 window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
   if (!audio) {
     return;
   } else {
     audio.currentTime = 0; // currentTime is used in the <audio> HTML tag to reset the audio whenever key is pressed.
     audio.play();
+    key.classList.add('playing');
   }
 
-  console.log(audio);
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  function removeTransition(keyPressed){
+    if (keyPressed.propertyName !== 'transform') {
+      return;
+    } else {
+      this.classList.remove('playing');
+    }
+  }
 });
