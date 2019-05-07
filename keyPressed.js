3275819afc7d235804fdbf5af4125214200a1c38
victor vinci <victorvinci@protonmail.com>
2019-05-07 18:56:37 +0200

    reset the time of the sound playing

--- keyPressed.js
+++ keyPressed.js
@@ -1,11 +1,12 @@
-window.addEventListener('keydown', function (event) {
+window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  
-  if(!audio) {
-    return
+
+  if (!audio) {
+    return;
   } else {
+    audio.currentTime = 0; // currentTime is used in the <audio> HTML tag to reset the audio whenever key is pressed.
     audio.play();
-  };
+  }
 
   console.log(audio);
-}); 
+});
